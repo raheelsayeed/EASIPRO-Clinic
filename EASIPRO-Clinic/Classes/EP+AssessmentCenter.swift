4f4c15e2bbf73494f1451a440307c1f36b29f036
Raheel Sayeed <raheel7@me.com>
2018-05-04 01:21:04 -0400

    Functional update

--- EASIPRO-Clinic/Classes/EP+AssessmentCenter.swift
+++ EASIPRO-Clinic/Classes/EP+AssessmentCenter.swift
@@ -7,3 +7,122 @@
 //
 
 import Foundation
+import EASIPRO
+import AssessmentCenter
+import ResearchKit
+import SMART
+
+extension AssessmentCenter.ACForm {
+	
+	func proMeasure() -> PROMeasure2 {
+		let title = self.title ?? OID
+		let prom = PROMeasure2(title: title, identifier: OID)
+		prom.measure = self
+		return prom
+	}
+	
+}
+
+class ACMeasureViewController : MeasuresViewController {
+	
+	open override func loadQuestionnaires() {
+		if nil != measures { return }
+		markBusy()
+		let acclient = ACClient.NewClient()
+		acclient.listForms { [unowned self] (acforms) in
+			if let acforms = acforms {
+				self._measures = acforms.map { $0.proMeasure() }
+				DispatchQueue.main.async {
+					self.markStandby()
+				}
+			}
+		}
+	}
+}
+
+extension EASIPRO.SessionController2 {
+	
+	open func prepareSessionContainer(callback: @escaping ((UIViewController?, Error?) -> Void)) {
+		guard let measures = measures else { return }
+		
+		
+		let context = SMARTManager.shared.usageMode
+		if context == .Practitioner {
+			
+			let acclient = ACClient.NewClient()
+			let acForms = measures.map({ (prom) -> ACForm in
+				return prom.measure as! ACForm
+			})
+			
+			acclient.forms(acforms: acForms, completion: { [weak self] (completedForms) in
+				if let completedForms = completedForms {
+					let taskViewControllers = completedForms.map({ (form) -> ACTaskViewController in
+						let acTVC = ACTaskViewController(acform: form, client: acclient, sessionIdentifier: (self?.patient.humanName!)!)
+						acTVC.taskDelegate = self
+						return acTVC
+					})
+					let navigationController = self?.sessionContainerController(for: taskViewControllers)
+					callback(navigationController, nil)
+				}
+				else {
+					callback(nil, nil)
+				}
+			})
+		}
+	}
+}
+
+
+extension EASIPRO.SessionController2 : ACTaskViewControllerDelegate {
+	
+	public func assessmentViewController(_ taskViewController: ACTaskViewController, didFinishWith reason: ORKTaskViewControllerFinishReason, error: Error?, tscore: Double?, stderror: Double?, session: SessionItem) {
+		
+		let acform = (taskViewController.task as! ACTask).form
+		
+		if reason == .completed { handleCompletion(for: acform, session: session) }
+		
+		
+	}
+	
+	public func didDismissACTaskViewController() {
+		
+	}
+	
+	func handleCompletion(for form: ACForm, session: SessionItem) {
+		var qr = form.as_FHIRQuestionnaireResponse(with: session.score)!
+		qr["subject"] = ["reference": "Patient/\(patient.id!.string)"]
+		qr["authored"] = "2018-01-17" // todo: change to today
+		do {
+			let qResponse = try QuestionnaireResponse(json: qr)
+			let srv = SMARTManager.shared.client.server
+			qResponse.createAndReturn(srv, callback: { [weak self] (ferror) in
+				let qResponseId = qResponse.id!.string
+				var observation = form.as_FHIRObservation(with: session.score!, related: qResponseId, subject: self?.patient.id!.string)
+				do {
+					let observationFHIR = try Observation(json: observation!)
+					observationFHIR.createAndReturn(SMARTManager.shared.client.server, callback: { (error) in
+						print("Observation:", observationFHIR.id?.string)
+						if let onMeasureCompletion = self?.onMeasureCompletion {
+							DispatchQueue.main.async {
+								onMeasureCompletion(form, nil)
+							}
+						}
+					})
+				}
+				catch {
+					print(error)
+				}
+			})
+
+			
+		}
+		catch { //do
+			print(error.localizedDescription)
+		}
+		
+
+
+		
+	}
+		
+}
--- EASIPRO-Clinic/Views/MainViewController.swift
+++ EASIPRO-Clinic/Views/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  MainViewController.swift
+//  MainViex`wController.swift
 //  EASIPRO-Clinic
 //
 //  Created by Raheel Sayeed on 5/3/18.
@@ -22,13 +22,11 @@ class MainViewController: UITableViewController {
             tableView.reloadData()
         }
     }
-    
-    
+	
     weak var patientHeaderView: PatientSectionHeader?
     
     weak final var btnBeginSession : UIButton?
 
-    
     var patientName : String? {
         get {
             return SMARTManager.shared.patient?.humanName
@@ -36,19 +34,15 @@ class MainViewController: UITableViewController {
     }
     
     var status : String = "" {
-        didSet { statusLabel?.text = status }
+		didSet {
+			weak var weakSelf = self
+			DispatchQueue.main.async {
+				weakSelf?.statusLabel?.text = weakSelf?.status
+			}
+		}
     }
     
     var statusLabel : UILabel?
-    
-    class func Today()->String {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .full
-        formatter.timeStyle = .none
-        return formatter.string(from: Date())
-    }
-
-
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -83,7 +77,7 @@ class MainViewController: UITableViewController {
         return measures?.count ?? 0
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PROMCell", for: indexPath)
         
         let measure = measures![indexPath.row]
         let completed = measure.sessionStatus == .completedCurrent
@@ -144,19 +138,25 @@ class MainViewController: UITableViewController {
         alertViewController.addAction(alertAction)
         present(alertViewController, animated: true, completion: nil)
     }
+	
+	
     func setupUI() {
-        
-        let profileBtn = UIBarButtonItem(image: UIImage.init(named: "icons8-male-user-filled-50"), style: .plain, target: self, action: #selector(showProfile))
+		
+		let formatter = DateFormatter()
+		formatter.dateStyle = .full
+		formatter.timeStyle = .none
+        let profileBtn = UIBarButtonItem(image: UIImage.init(named: "profileicon"), style: .plain, target: self, action: #selector(showProfile))
         let practitionerBtn = UIBarButtonItem(title: "LOGIN", style: .plain, target: self, action: #selector(showProfile))
         navigationItem.rightBarButtonItems = [profileBtn, practitionerBtn]
-        navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: MainViewController.Today(), style: .plain, target: nil, action: nil)
+        navigationItem.leftBarButtonItem = UIBarButtonItem.init(title: formatter.string(from: Date()), style: .plain, target: nil, action: nil)
         let flexibleItem = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        statusLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        statusLabel = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 40))
         statusLabel?.text = "Ready"
         statusLabel?.textAlignment = .center
+		statusLabel?.textColor = .white
+		statusLabel?.font = UIFont(descriptor: .preferredFontDescriptor(withTextStyle: .title3), size: 15)
         let labelItem = UIBarButtonItem(customView: statusLabel!)
-        let proHistory = UIBarButtonItem.init(title: "PRO-History", style: .plain, target: nil, action: nil)
-        toolbarItems = [proHistory, flexibleItem, labelItem, flexibleItem]
+        toolbarItems = [flexibleItem, labelItem, flexibleItem]
         
     }
     
@@ -174,7 +174,7 @@ class MainViewController: UITableViewController {
     func accessoryView() -> UIButton {
         
         let btn = UIButton(type: .roundedRect)
-        let img = UIImage.init(named: "icons8-ok-50")
+        let img = UIImage.init(named: "greencheckmark")
         let imgView = UIImageView(image: img)
         imgView.frame = CGRect(x: 0, y: 0, width: 25, height: 25)
         btn.setBackgroundImage(img, for: .normal)
@@ -184,17 +184,18 @@ class MainViewController: UITableViewController {
     }
     
     @objc public func selectMeasures(_ sender: UIButton) {
-//        let measuresViewController = ACMeasuresViewController()
-//        measuresViewController.onSelection = { (measures) in
-//            self.measures = measures
-//        }
-//        present(popUpNavigationController(root: measuresViewController, frame: sender.frame), animated: true, completion: nil)
+        let measuresViewController = ACMeasureViewController()
+        measuresViewController.onSelection = { (measures) in
+            self.measures = measures
+        }
+        present(popUpNavigationController(root: measuresViewController, frame: sender.frame), animated: true, completion: nil)
     }
     
     @objc func showPROHistory(_ sender: Any) {
         let insights = InsightsController(style: .grouped)
         insights.view.backgroundColor = view.backgroundColor
         let navigationController = UINavigationController(rootViewController: insights)
+		navigationController.navigationBar.barStyle = .black
         self.present(navigationController, animated: true, completion: nil)
     }
     
@@ -219,7 +220,7 @@ class MainViewController: UITableViewController {
     
     @objc public func beginSession(_ sender: Any?) {
         
-        guard let pt = SMARTManager.shared.patient, let measures = measures, let practitioner = SMARTManager.shared.practitioner else {
+        guard let pt = SMARTManager.shared.patient,  let practitioner = SMARTManager.shared.practitioner, let measures = measures else {
             showMsg(msg: "Please login and select Patient Profile")
             return
         }
@@ -236,14 +237,16 @@ class MainViewController: UITableViewController {
         }
         
         sessionController?.onMeasureCompletion = { [weak self] result, measure in
-            
-            let filtered = self?.measures?.filter { $0 == measure }.first
-            if let idx = filtered {
-                idx.sessionStatus = .completedCurrent
-                self?.status = "Session Completed for \(self?.patientName ?? "---")"
-            }
-            btn?.reset()
-            self?.tableView.reloadData()
+			
+			if let result = result as? ACForm {
+				let filtered = self?.measures?.filter { $0.identifier == result.OID }.first
+				if let idx = filtered {
+					idx.sessionStatus = .completedCurrent
+					self?.status = "Session Completed for \(self?.patientName ?? "---")"
+				}
+				btn?.reset()
+				self?.tableView.reloadData()
+			}
         }
         
         sessionController?.prepareSessionContainer { [weak self] (viewController, error) in
@@ -253,69 +256,25 @@ class MainViewController: UITableViewController {
                 btn?.reset()
                 return
             }
-            
             viewController.view.tintColor = UIColor.red
             self?.present(viewController, animated: true, completion: nil)
         }
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-     override func tableView(_ tab    var status : String = "" {
-     didSet { statusLabel?.text = status }
-     }
-     
-     var statusLabel : UILabel?leView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
+extension MainViewController : UIPopoverPresentationControllerDelegate{
+	
+	func popUpNavigationController(root: UIViewController, frame:CGRect) -> UINavigationController {
+		let navigationController = UINavigationController(rootViewController: root)
+		navigationController.modalPresentationStyle = UIModalPresentationStyle.popover
+		navigationController.popoverPresentationController?.sourceView = self.view
+		navigationController.popoverPresentationController?.sourceRect = frame
+		navigationController.popoverPresentationController?.delegate = self
+		return navigationController
+	}
+	
+	func adaptivePresentationStyle(for controller: UIPresentationController, traitCollection: UITraitCollection) -> UIModalPresentationStyle {
+		return .none
+	}
+}
+
