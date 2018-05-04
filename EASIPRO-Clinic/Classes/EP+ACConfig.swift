69b90c46734787fe9f4f24a53bf13450517dd62c
Raheel Sayeed <raheel7@me.com>
2018-05-04 01:18:29 -0400

    configs

---
+++ EASIPRO-Clinic/Classes/EP+ACConfig.swift
@@ -0,0 +1,9 @@
+//
+//  EP+ACConfig.swift
+//  EASIPRO-Clinic
+//
+//  Created by Raheel Sayeed on 03/05/18.
+//  Copyright © 2018 Boston Children's Hospital. All rights reserved.
+//
+
+import Foundation
